2f81435ca9fbaa3ff60ffc94cd9c03c054eaf53d
Mikael Verdu <mikael.verdu@gmail.com>
2019-02-20 23:01:44 +0100

    add mysql package & connect API to database done

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const app = express()
 const router = express.Router()
 const settings = require('./settings')
 const routes = require('./routes')
+const mysql = require('mysql')
+
+const connection = mysql.createConnection(settings.database)
 
 router.get('/employees', routes.employees.listAllEmployees)
 
 app.use('/api', router)
 
-app.listen(settings.APIServerPort, () => console.log(`Listening on port ${settings.APIServerPort}`))
+connection.connect(error => {
+  if (error) {
+    console.log('Error connecting to the database', error)
+    return process.exit()
+  }
+  app.listen(settings.APIServerPort, () => console.log(`Listening on port ${settings.APIServerPort}`))
+})
