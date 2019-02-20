479b4cbffa19a17440f8aa6965c3e72e2aa40be7
Mikael Verdu <mikael.verdu@gmail.com>
2019-02-20 21:41:24 +0100

    add entry point app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+const PORT = 8080
+
+app.get('/', (req, res) => res.send('Connected'))
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
