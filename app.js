282663f86e9bd30c2143ff62c8fa8882e7f98b34
Mikael Verdu <mikael.verdu@gmail.com>
2019-02-20 22:38:39 +0100

    add settings file

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
-const PORT = 8080
+const settings = require('./settings')
 const routes = require('./routes')
 
 router.get('/employees', routes.employees.listAllEmployees)
 
 app.use('/api', router)
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(settings.APIServerPort, () => console.log(`Listening on port ${settings.APIServerPort}`))
